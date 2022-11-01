bd2afc067324274ffe882f484b3af70b057f8759
Michal-Wrona <michalwro112@gmail.com>
2022-11-01 20:22:04 +0100

    Dodaj pliki projektów.

---
+++ WebApplication21.Core/Data/WebApplication21DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication21.Core.Entities;
+
+namespace WebApplication21.Core.Data
+{
+    public class WebApplication21DbContext : DbContext
+    {
+        public WebApplication21DbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
