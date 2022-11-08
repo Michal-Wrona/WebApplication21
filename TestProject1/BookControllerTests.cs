e7f2c52daddc50d9f22080153ba5106ba83cd826
Michal-Wrona <michalwro112@gmail.com>
2022-11-08 15:32:16 +0100

    Add test

---
+++ TestProject1/BookControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using FluentAssertions;
+
+namespace WebApplication21.Tests
+{
+    public class BookControllerTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnOkResult()
+        {
+            //arrange(
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            //act
+            var response = await client.GetAsync("/api/book");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
---
+++ WebApplication21.Tests/IntegrationTests/BookControllerTests/BookControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace WebApplication21.Tests.IntegrationTests.BookControllerTests
+{
+    public class BookControllerTest
+    {
+        [Fact]
+        public async Task GetAll()
+        {
+            //arrange
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            //act
+            var response = await client.GetAsync("/api/book");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
